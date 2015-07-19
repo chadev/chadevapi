c6fe92e4f55154da6ecf71abc5ea402825a5aa19
Chris Keathley <c@keathley.io>
2015-07-19 15:30:28 -0400

    Pull data from meetup and enable cors

--- api.rb
+++ api.rb
@@ -1,5 +1,69 @@
 require 'sinatra'
 
+require 'net/http'
+require 'json'
+require 'rack/cors'
+
+BASE_URL = "https://api.meetup.com/"
+API_KEY  = ENV.fetch('MEETUP_API_KEY')
+
+use Rack::Cors do |config|
+  config.allow do |allow|
+    allow.origins '*'
+    allow.resource '/lunches', headers: :any
+  end
+end
+
 get '/lunches' do
-  'hello world'
+  lunch_hashes = Meetup.events.map { |json| Event.new(json) }.map(&:to_h)
+  lunch_hashes.to_json
+end
+
+class Meetup
+  def self.events(group_name: 'chadevs', status: 'upcoming')
+    @events = get_json_data(
+      '2/events',
+      group_urlname: group_name,
+      status: status,
+      page: 100
+    )
+  end
+
+  def self.get_json_data(resource, params)
+    uri = initial_uri(resource, params)
+    json = fetch_data(uri)
+    json[:results]
+  end
+
+  def self.initial_uri(resource, params)
+    uri = URI("#{BASE_URL}/#{resource}")
+    uri.query = URI.encode_www_form(params.merge({ key: API_KEY }))
+    uri
+  end
+
+  def self.fetch_data(uri)
+    res  = Net::HTTP.get_response(uri)
+    JSON.parse(res.body, symbolize_names: true)
+  end
+end
+
+class Event
+  attr_reader :id, :name, :time, :description
+
+  def initialize(json)
+    @event_json   = json
+    @id           = @event_json.fetch(:id)
+    @name         = @event_json.fetch(:name)
+    @time         = @event_json.fetch(:time)
+    @description  = @event_json.fetch(:description) { '' }
+  end
+
+  def to_h
+    {
+      id: @id,
+      name: @name,
+      time: @time,
+      description: @description,
+    }
+  end
 end
